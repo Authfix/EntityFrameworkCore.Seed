5978b412fad3e4d49576a468a0acf5252b0b41b7
Authfix <3940727+Authfix@users.noreply.github.com>
2017-10-30 16:38:00 +0100

    chore(app): add AssemblyInfo
    Add AssemblyInfo manually to be compatible with GitVersion Task on vsts

---
+++ src/EFCore.Seed/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Daztane.EntityFrameworkCore.Seed")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Daztane.EntityFrameworkCore.Seed")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("68ad71fc-904b-4c95-9ebd-0d9b77ae68b8")]
