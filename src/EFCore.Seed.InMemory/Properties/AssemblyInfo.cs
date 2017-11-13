49467769e7183b0716d18275098726ce96274508
Authfix <3940727+Authfix@users.noreply.github.com>
2017-11-13 23:59:40 +0100

    fix(in-memory): disable AssemblyInfo generation
    Disable the AssemblyInfo.cs file generation for continuous deployment purpose.

--- src/EFCore.Seed.InMemory/Properties/AssemblyInfo.cs
+++ src/EFCore.Seed.InMemory/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Authfix.EntityFrameworkCore.Seed.Postgres")]
-[assembly: AssemblyDescription("Postgres provider for Authfix.EntityFrameworCore.Seed")]
+[assembly: AssemblyTitle("Authfix.EntityFrameworkCore.Seed.InMemory.Properties")]
+[assembly: AssemblyDescription("InMemory provider for Authfix.EntityFrameworkCore.Seed")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Authfix.EntityFrameworkCore.Seed.Postgres")]
+[assembly: AssemblyProduct("Authfix.EntityFrameworkCore.Seed.InMemory.Properties")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6ccd2d96-0b5b-454b-810a-1c8249f0fe9b")]
+[assembly: Guid("4e1bbb50-208b-499a-aedd-f7a15f186153")]
