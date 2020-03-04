56db0e7f17d7212e6be122a9373148ff5a4f9e4f
Thomas Bailly <3940727+Authfix@users.noreply.github.com>
2020-03-04 15:10:26 +0100

    chore(lib): add host for seed option entry class

--- src/EFCore.Seed/Extensions/WebhostExtensions.cs
+++ src/EFCore.Seed/Extensions/WebhostExtensions.cs
@@ -3,10 +3,11 @@
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.  
 //
 
-using Authfix.EntityFrameworkCore.Seed.Extensions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace Authfix.EntityFrameworkCore.Seed.Extensions
 {
@@ -18,14 +19,34 @@ namespace Authfix.EntityFrameworkCore.Seed.Extensions
         /// <param name="webHost">The web host.</param>
         public static IWebHost SeedData<T>(this IWebHost webHost) where T : DbContext
         {
-            using (var serviceScope = webHost.Services.GetService<IServiceScopeFactory>().CreateScope())
+            Seed(webHost.Services, typeof(T));
+
+            return webHost;
+        }
+
+        /// <summary>
+        /// Seeds the data for a specific <see cref="DbContext"/>.
+        /// </summary>
+        /// <param name="host">The host.</param>
+        public static IHost SeedData<T>(this IHost host) where T : DbContext
+        {
+            Seed(host.Services, typeof(T));
+
+            return host;
+        }
+
+        /// <summary>
+        /// Seed the current database
+        /// </summary>
+        /// <param name="serviceProvider"></param>
+        private static void Seed(IServiceProvider serviceProvider, Type dbContextType)
+        {
+            using(var serviceScope = serviceProvider.CreateScope())
             {
-                var appContext = serviceScope.ServiceProvider.GetService<T>();
+                var appContext = serviceScope.ServiceProvider.GetService(dbContextType) as DbContext;
 
                 appContext.Database.Seed();
             }
-
-            return webHost;
         }
     }
 }
